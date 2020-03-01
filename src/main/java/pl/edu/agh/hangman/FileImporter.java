0bc318ecfe89bded0225aa9713f59a2af647fc82
Michal Binduga <student22@pcoip.ki.agh.edu.pl>
2020-03-01 14:23:18 +0100

    Another Next Stage of ImportFile class

--- src/main/java/pl/edu/agh/hangman/FileImporter.java
+++ src/main/java/pl/edu/agh/hangman/FileImporter.java
@@ -7,18 +7,19 @@ import java.util.Scanner;
 
 public class FileImporter {
 	File file = new File("C:\\Users\\student22\\Desktop\\Hangman\\java-hangman\\src\\main\\resources");
-	ArrayList<String> List = new ArrayList<>();
+	ArrayList<String> list = new ArrayList<>();
 
-	public List<String> Convert(File file) throws FileNotFoundException { 
+	public List<String> Convert() throws FileNotFoundException { 
 		Scanner scan;
 		scan = new Scanner(new BufferedReader(new FileReader(file)));
-		List.add(scan.nextLine());
-		return List;
+		list.add(scan.nextLine());
+		scan.close();
+		return list;
 	}
 	
-	public String Losowanie(List<String> list) {
-		int randomIndex = (int) (Math.random() * list.size());
-		return list.get(randomIndex);
+	public String Losowanie() {
+		int randomIndex = (int) (Math.random() * this.list.size());
+		return this.list.get(randomIndex);
 	}
 	
 }
